32f76a639d802b35e524946510921953d1e00c46
Dayllan de Souza Alho <dayllan.alho@sou.inteli.edu.br>
2022-06-06 18:44:24 -0300

    :sparkles: Subindo telas reorganizadas do Front-End

--- src/Frontend/Sequencia/9. HOTEL.DAYLLAN - Agendamento de Antecipação/JS/Agendamento-de-antecipacao.js
+++ src/Frontend/Sequencia/9. HOTEL.DAYLLAN - Agendamento de Antecipação/JS/Agendamento-de-antecipacao.js
@@ -1,4 +1,4 @@
 
 function changePage1(){
-    window.location='file:///C:/Users/Inteli/Desktop/DESENVOLVIMENTO%20WEB/NOVAS%20TELAS%20-%20DAYLLAN/Apresenta%C3%A7%C3%A3o/10.%20HOTEL.DAYLLAN%20-%20Agendamento%20finalizado/HTML/Agendamento%20Finalizado.html'
+    window.location='file:///C:/Users/Inteli/Documents/GitHub/2022.M2.T4-PROJETO4/src/Frontend/Sequencia/10.%20HOTEL.DAYLLAN%20-%20Agendamento%20finalizado/HTML/Agendamento%20Finalizado.html'
 }
