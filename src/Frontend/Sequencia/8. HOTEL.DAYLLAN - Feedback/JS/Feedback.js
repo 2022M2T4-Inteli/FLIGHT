24345bb15c792c65182f02e7b6425488853e7995
Dayllan de Souza Alho <dayllan.alho@sou.inteli.edu.br>
2022-06-07 15:17:33 -0300

    :sparkles: Update FrontEnd corrigido

--- src/Frontend/Sequencia/8. HOTEL.DAYLLAN - Feedback/JS/Feedback.js
+++ src/Frontend/Sequencia/8. HOTEL.DAYLLAN - Feedback/JS/Feedback.js
@@ -1,4 +1,4 @@
 
 function changePage(){
-    window.location='file:///C:/Users/Inteli/Desktop/DESENVOLVIMENTO%20WEB/NOVAS%20TELAS%20-%20DAYLLAN/HOTEL.JORDAN%20-%20Conta/HTML/mainHoteleiro.html'
+    window.location='file:///C:/Users/Inteli/Documents/GitHub/2022.M2.T4-PROJETO4/src/Frontend/Sequencia/1.%20HOTEL.JORDAN%20-%20Conta/HTML/mainHoteleiro.html'
 }
