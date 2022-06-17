d4efb25944ab7ad9a45a67775d13c78b1c0b330e
Dayllan de Souza Alho <dayllan.alho@sou.inteli.edu.br>
2022-06-17 11:15:00 -0300

    :sparkles: Add da pasta src

---
+++ src/Frontend/mainHoteleiro.js
@@ -0,0 +1,51 @@
+var texto_original = "**********";
+function openEye(){
+    if(document.getElementById("teste").src=="http://127.0.0.1:5501/olho_aberto2.png")
+    {
+        console.log("passei aqui!");
+        document.getElementById("teste").src = "olho_fechado.png";
+        texto_original=document.getElementById("teste2").innerText;
+        document.getElementById("teste2").innerText = "*******";
+    }
+    else
+    {
+        document.getElementById("teste").src = "olho_aberto2.png";
+        document.getElementById("teste2").innerText = texto_original;
+    }
+   
+}
+function openMenu() {
+    window.location = 'Sequencia/MenuHoteleiro/HTML/menu.html'
+}
+
+function menu(){    
+    window.location = 'file:///C:/Users/Inteli/Desktop/DESENVOLVIMENTO%20WEB/NOVAS%20TELAS%20-%20DAYLLAN/Apresenta%C3%A7%C3%A3o/2.%20HOTEL.HENRI%20-%20Menu.Hoteleiro/HTML/tela%20de%20cadastro.html'
+}
+
+function antecipe_seus_ganhos(){
+    window.location= 'Sequencia/Fatura/HTML/SolicitacaoHurb.html'
+}
+
+function antecipacao_automatica(){
+    window.location= 'Sequencia/Agendamento/HTML/Agendamento.html'
+}
+
+function extrato(){
+    window.location=''
+}
+function configuracoes(){
+    window.location = ''
+}
+
+function saiba_mais(){
+    window.location= 'file:///C:/Users/Inteli/Desktop/DESENVOLVIMENTO%20WEB/NOVAS%20TELAS%20-%20DAYLLAN/Apresenta%C3%A7%C3%A3o/2.%20HOTEL.HENRI%20-%20Menu.Hoteleiro/HTML/tela%20de%20cadastro.html'
+}
+
+function getAmount() {
+    $.get("http://127.0.0.1:3000/get-access", function(access) {
+        let logged = access[0].login_parceiro;
+        $.get("http://127.0.0.1:3000/users", function(users) {
+            
+        })})
+
+    }
